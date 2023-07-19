bd585a189f63ed604e886b0e5bbd1afc9b79e8e4
Decio Ferreira <decio_ferreira1@hotmail.com>
2023-07-18 22:04:50 -0300

    TF Files

---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "aws_cloudwatch_retention_in_days" {
+  type        = number
+  description = "AWS CloudWatch Logs Retention in Days"
+  default     = 1
+}
+
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+}
+
+variable "app_environment" {
+  type        = string
+  description = "Application Environment"
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "Lista de CIDR Blocks para as sub-redes públicas"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Lista de CIDR Blocks para as sub-redes privadas"
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+}  
+
+variable "nginx_container_image" {
+  description = "URL da imagem do nginx no ECR"
+
+}
