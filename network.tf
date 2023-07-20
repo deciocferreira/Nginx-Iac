674be324a599eb1f5e31e657becc6f8d2c2c0171
Decio Ferreira <decio_ferreira1@hotmail.com>
2023-07-20 15:16:23 -0300

    Network changes

--- network.tf
+++ network.tf
@@ -35,6 +35,38 @@ resource "aws_subnet" "private" {
   }
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "aws-igw" {
+  vpc_id = aws_vpc.aws-vpc.id
+  tags = {
+    Name        = "${var.app_name}-igw"
+    Environment = var.app_environment
+  }
+
+}
+
+# Tabelas de roteamento público
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  tags = {
+    Name        = "${var.app_name}-routing-table-public"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.aws-igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
 # NAT Gateway 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
